b7a07ba2e838c8e024ac2bcbbd7b165e2a42728c
KPA927 <55092605+KPA927@users.noreply.github.com>
2019-09-11 22:29:53 +0300

    Add files via upload
    К сожалению, я не могу использовать функцию home() меньше двух раз...

--- lab1/task_19.py
+++ lab1/task_19.py
@@ -5,21 +5,20 @@ from pyrob.api import *
 
 @task
 def task_8_29():
-    def home():
-        while not wall_is_above():
-            move_up()
-        while not wall_is_on_the_left():
-            move_left()
-    while wall_is_above() and wall_is_beneath() and not wall_is_on_the_left():
+    while not wall_is_on_the_left():
         move_left()
     if not wall_is_above():
         home()
     else:
-            while wall_is_above() and wall_is_beneath() and not wall_is_on_the_right():
-                move_right()
-            if not wall_is_above():
-                home()
-            
+        while not wall_is_on_the_right():
+            move_right()
+        if not wall_is_above():
+            home()
+def home():
+    while not wall_is_above():
+        move_up()
+    while not wall_is_on_the_left():
+        move_left()
 
 
 if __name__ == '__main__':
