5b2802d796fe1d3efcd6504e3a035f639c5b2f6c
KPA927 <55092605+KPA927@users.noreply.github.com>
2019-09-13 13:04:26 +0300

    Add files via upload

--- lab1/task_18.py
+++ lab1/task_18.py
@@ -6,20 +6,14 @@ from pyrob.api import *
 @task
 def task_8_28():
     while not wall_is_on_the_left():
-        if wall_is_above() and wall_is_beneath():
-            move_left()
-        else:
-            home()
-    if not wall_is_above or not wall_is_beneath() and not wall_is_on_the_left():
-        home()
+        move_left()
     while True:
         if wall_is_above() and wall_is_beneath():
             move_right()
         else:
             home()
             break
-    if not wall_is_above or not wall_is_beneath() and not wall_is_on_the_left():
-        home() 
+
 
 
 def home():
