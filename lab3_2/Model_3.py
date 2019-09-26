08892c29612b988fa75777794c2d81ef12d30b19
KPA927 <kamenskikh.pasha@mail.ru>
2019-09-27 01:12:51 +0300

    draw_cloud was written, part_of_sky was added

--- lab3_2/Model_3.py
+++ lab3_2/Model_3.py
@@ -35,7 +35,7 @@ def draw_background(w):
     sea.draw(w)
     sand.draw(w)
     draw_raft(0)
-    draw_raft(SIZE_X*7/8)
+    draw_raft(SIZE_X*6/8)
 
 
 def draw_ball(w):
@@ -47,27 +47,44 @@ def draw_ball(w):
 
 
 def draw_raft(x):
-    raft = gr.Rectangle(gr.Point(x, SIZE_Y/2 - 10), gr.Point(x+100, SIZE_Y/2+20))
+    raft = gr.Rectangle(gr.Point(x, SIZE_Y/2 - 10), gr.Point(x+200, SIZE_Y/2+20))
     raft.setFill('Brown')
     raft.setOutline('Brown')
 
     for i in range(5):
-        draw_log(20*i+10+x)
+        draw_log(40*i+20+x)
     raft.draw(w)
 
+
 def draw_log(x):
-    log = gr.Circle(gr.Point(x, SIZE_Y/2+30), 10)
+    log = gr.Circle(gr.Point(x, SIZE_Y/2+40), 20)
     log.setFill('Sienna3')
     log.setOutline('Brown')
 
     log.draw(w)
 
 def draw_sun(w):
-    pass
+    sun = gr.Circle(gr.Point(740, 60), 60)
+    sun.setFill("Yellow")
+    sun.setOutline('Orange')
+
+    sun.draw(w)
 
 
-def draw_cloud(w):
-    pass
+def draw_cloud(x, y):
+    for i in range(4):
+        part_of_cloud(x+i*30, y, 15)
+        part_of_cloud(x+i*30 + 15, y+15, 20)
+        part_of_cloud(x+i*30 + 15, y-15, 20)
+    part_of_cloud(x+120, y, 15)
+
+
+def part_of_cloud(x, y, r):
+    circle = gr.Circle(gr.Point(x, y), r)
+    circle.setOutline('White')
+    circle.setFill('White')
+
+    circle.draw(w)
 
 
 def draw_fish(w):
@@ -97,7 +114,8 @@ def check_floor():
 def decoration(w):
     draw_background(w)
     draw_sun(w)
-    draw_cloud(w)
+    draw_cloud(100, 110)
+    draw_cloud(50, 40)
     draw_ball(w)
 
 
