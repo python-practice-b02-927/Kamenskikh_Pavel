f7a91da5b39a26d40a0cce65a31c6159d53df8fc
KPA927 <kamenskikh.pasha@mail.ru>
2019-10-08 14:55:30 +0300

    Function move() was added, now balls can move, click() doesn't work

--- lab6/Test4.py
+++ lab6/Test4.py
@@ -4,8 +4,8 @@ import time
 root = Tk()
 root.geometry('800x600')
 
-canv = Canvas(root,bg='white')
-canv.pack(fill=BOTH,expand=1)
+canv = Canvas(root, bg='white')
+canv.pack(fill=BOTH, expand=1)
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue']
 l = Label(bg='black', fg='white', width=20)
@@ -13,7 +13,9 @@ l.pack()
 score = 0
 s = 'Score: '
 
+Number_balls = 0
 balls = []
+
 
 def new_ball():
     global balls
@@ -23,34 +25,47 @@ def new_ball():
     y = d['y'] = rnd(100, 500)
 
     r = d['r'] = rnd(30, 50)
-    d['v_y'] = rnd(0, 20)
 
-    d['v_x'] = rnd(0, 20)
-    d['id'] = canv.create_oval(x-r, y-r, x+r, y+r, fill = choice(colors), width=0)
+    d['color'] = choice(colors)
+    d['v_y'] = rnd(-20, 20)
+    d['v_x'] = rnd(-20, 20)
+
+    d['id'] = canv.create_oval(x-r, y-r, x+r, y+r, fill = d['color'], width=0)
 
     balls.append(d)
 
-    root.after(1000, new_ball)
-
+    root.after(2000, new_ball)
     l['text'] = '' + s + str(score)
 
 
-def update_velocity(i):
-    balls[i['x']] = + balls[i['v_x']]
-    balls[i['y']] = + balls[i['v_y']]
+def update_coords(i):
+    balls[i]['x'] +=  balls[i]['v_x']
+    balls[i]['y'] += balls[i]['v_y']
 
 
-#def click(event, i):
- #   global score
-  #  if ((event.x - balls[i['x']]) ** 2 + (event.y - balls[i['y']]) ** 2) ** (1 / 2) <= balls[i['r']]:
-   #     score += 1
-    #else:
-    #    canv.create_line(event.x - 5, event.y + 5, event.x + 5, event.y - 5, fill='red', width=3)
-     #   canv.create_line(event.x + 5, event.y + 5, event.x - 5, event.y - 5, fill='red', width=3)
+def move():
+    canv.delete(ALL)
+
+    for i in range(len(balls)):
+        update_coords(i)
+        canv.create_oval(balls[i]['x'] - balls[i]['r'], balls[i]['y'] - balls[i]['r'], balls[i]['x'] + balls[i]['r'],
+                         balls[i]['y'] + balls[i]['r'], fill = balls[i]['color'], width=0)
+
+    root.after(1000, move)
+
+
+def click(event, i):
+    global score
+    if ((event.x - balls[i]['x']) ** 2 + (event.y - balls[i]['y']) ** 2) ** 0.5 <= balls[i]['r']:
+        score += 1
+    else:
+        canv.create_line(event.x - 5, event.y + 5, event.x + 5, event.y - 5, fill='red', width=3)
+        canv.create_line(event.x + 5, event.y + 5, event.x - 5, event.y - 5, fill='red', width=3)
 
 def main():
     new_ball()
-    #canv.bind('<Button-1>', click)
+    move()
+    canv.bind('<Button-1>', click)
 
 main()
 
