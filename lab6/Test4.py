f174b3ad6a2a2a10febb8fbd08c7e25b4425a55a
KPA927 <kamenskikh.pasha@mail.ru>
2019-10-10 21:56:12 +0300

    New figures were added which give more points and there are no errors.

--- lab6/Test4.py
+++ lab6/Test4.py
@@ -13,25 +13,39 @@ l.pack()
 score = 0
 s = 'Score: '
 
+All_Number = 0
 Number_balls = 0
 balls = []
-k = -1
 
 
 def new_ball():
-    global balls
+    global balls, All_Number, d
+    All_Number += 1
     d = {}
 
-    x =d['x'] = rnd(100, 700)
+    x = d['x'] = rnd(100, 700)
     y = d['y'] = rnd(100, 500)
-
     r = d['r'] = rnd(20, 40)
 
     d['color'] = choice(colors)
-    d['v_y'] = rnd(-20, 20)
-    d['v_x'] = rnd(-20, 20)
+    d['v_y'] = rnd(-4, 4)
+    d['v_x'] = rnd(-4, 4)
 
-    d['id'] = canv.create_oval(x-r, y-r, x+r, y+r, fill = d['color'], width=0)
+    a = All_Number % 30
+    b = All_Number % 3
+    if a == 0:
+        d['id'] = canv.create_oval(x - r, y - r, x + r, y + r, fill='yellow', width=0)
+        canv.create_arc(x - 0.6 * r, y - 0.6 * r, x + 0.6 * r, y + 0.6 * r,
+                     start=220, extent=100, style=ARC, outline='red', width=5)
+        canv.create_oval(x - 0.8 * r, y - 0.6 * r, x - 0.6 * r, y - 0.4 * r, fill='black', width=0)
+        canv.create_oval(x + 0.8 * r, y - 0.6 * r, x + 0.6 * r, y - 0.4 * r, fill='black', width=0)
+        d['shape'] = 'smile'
+    elif b == 0:
+        d['id'] = canv.create_rectangle(x - r, y - r, x + r, y + r, fill=d['color'], width=0)
+        d['shape'] = 'rectangle'
+    else:
+        d['id'] = canv.create_oval(x - r, y - r, x + r, y + r, fill=d['color'], width=0)
+        d['shape'] = 'circle'
 
     balls.append(d)
 
@@ -41,39 +55,62 @@ def new_ball():
 
 def update_coords(i):
     check_walls(i)
-    balls[i]['x'] +=  balls[i]['v_x']
+    balls[i]['x'] += balls[i]['v_x']
     balls[i]['y'] += balls[i]['v_y']
 
 
 def check_walls(i):
-    if balls[i]['x'] >= 800 or balls[i]['x'] <= 0 or balls[i]['y'] >= 600 or balls[i]['y'] <= 0:
-        balls[i]['v_x'] = rnd(-20, 20)
-        balls[i]['v_y'] = rnd(-20, 20)
+    if balls[i]['x'] >= 800 - balls[i]['r'] or balls[i]['x'] <= balls[i]['r']:
+        balls[i]['v_x'] = - balls[i]['v_x']
+
+    if balls[i]['y'] >= 600 - balls[i]['r'] or balls[i]['y'] <= balls[i]['r']:
+        balls[i]['v_y'] = - balls[i]['v_y']
 
 
 def move():
     canv.delete(ALL)
-
-    for i in range(len(balls)):
+    Number_balls = len(balls)
+    for i in range(Number_balls):
         update_coords(i)
-        canv.create_oval(balls[i]['x'] - balls[i]['r'], balls[i]['y'] - balls[i]['r'], balls[i]['x'] + balls[i]['r'],
-                         balls[i]['y'] + balls[i]['r'], fill = balls[i]['color'], width=0)
-    root.after(1000, move)
+        x = balls[i]['x']
+        y = balls[i]['y']
+        r = balls[i]['r']
+        if balls[i]['shape'] == 'smile':
+            canv.create_oval(x - r, y - r, x + r, y + r, fill='yellow', width=0)
+            canv.create_arc(x - 0.6 * r, y - 0.6 * r, x + 0.6 * r, y + 0.6 * r,
+                            start=220, extent=100, style=ARC, outline='red', width=5)
+            canv.create_oval(x - 0.8 * r, y - 0.6 * r, x - 0.6 * r, y - 0.4 * r, fill='black', width=0)
+            canv.create_oval(x + 0.8 * r, y - 0.6 * r, x + 0.6 * r, y - 0.4 * r, fill='black', width=0)
+        elif balls[i]['shape'] == 'rectangle':
+            canv.create_rectangle(x - r, y - r, x + r, y + r, fill=balls[i]['color'], width=0)
+        else:
+            canv.create_oval(x - r, y - r, x + r, y + r, fill=balls[i]['color'], width=0)
+    root.after(20, move)
 
 
 def click(event):
     global score
-    for i in range(len(balls)):
-        if ((event.x - balls[i]['x']) ** 2 + (event.y - balls[i]['y']) ** 2) ** 0.5 <= balls[i]['r']:
-            score += 1
-            k = i
-            break
-    else:
-        canv.create_line(event.x - 5, event.y + 5, event.x + 5, event.y - 5, fill='red', width=3)
-        canv.create_line(event.x + 5, event.y + 5, event.x - 5, event.y - 5, fill='red', width=3)
+    delete = []
 
-    if k >= 0:
-        elem = balls.pop(k)
+    for i in range(len(balls)):
+        R = ((event.x - balls[i]['x']) ** 2 + (event.y - balls[i]['y']) ** 2) ** 0.5
+
+        if R <= balls[i]['r'] and balls[i]['shape'] == 'circle':
+            delete.append(i)
+            score += 1
+
+        if R <= balls[i]['r'] and balls[i]['shape'] == 'rectangle':
+            delete.append(i)
+            score += 10
+
+        if R <= balls[i]['r'] and balls[i]['shape'] == 'smile':
+            delete.append(i)
+            score *= 2
+
+    Number_balls = len(delete)
+    for i in range(Number_balls):
+        canv.delete(balls[delete[i]]['id'])
+        del balls[delete[i]]
 
 
 def main():
