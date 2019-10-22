e4d4e46267cf6cdc1b6f58276f2ba6bbdd25d34f
KPA927 <kamenskikh.pasha@mail.ru>
2019-10-23 00:44:26 +0300

    Add check_walls() and update balls.move()

--- lab8/gun.py
+++ lab8/gun.py
@@ -10,6 +10,7 @@ fr = tk.Frame(root)
 root.geometry('800x600')
 canv = tk.Canvas(root, bg='white')
 canv.pack(fill=tk.BOTH, expand=1)
+k = 0.8
 
 
 class Ball:
@@ -22,26 +23,30 @@ class Ball:
         """
         self.x = x
         self.y = y
+
         self.r = 10
+
         self.vx = 0
         self.vy = 0
+        self.a = 2
+
         self.color = choice(['blue', 'green', 'red', 'brown'])
         self.id = canv.create_oval(
-                self.x - self.r,
-                self.y - self.r,
-                self.x + self.r,
-                self.y + self.r,
-                fill=self.color
+            self.x - self.r,
+            self.y - self.r,
+            self.x + self.r,
+            self.y + self.r,
+            fill=self.color
         )
         self.live = 30
 
     def set_coord(self):
         canv.coords(
-                self.id,
-                self.x - self.r,
-                self.y - self.r,
-                self.x + self.r,
-                self.y + self.r
+            self.id,
+            self.x - self.r,
+            self.y - self.r,
+            self.x + self.r,
+            self.y + self.r
         )
 
     def move(self):
@@ -51,18 +56,33 @@ class Ball:
         self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
         и стен по краям окна (размер окна 800х600).
         """
-        # FIXME
-        self.vy -= 1
+        self.vy -= self.a
+
         self.x += self.vx
         self.y -= self.vy
-        if self.x >= 800 - self.r or self.x <= self.r:
-            self.vx = -self.vx
-        if self.y >= 600 - self.r or self.y <= self.r:
-            self.vy = -self.vy
+        self.check_walls()
         self.set_coord()
 
-    def hit_it(self, obj):
+    def check_walls(self, num_hits=0):
+        if self.x >= 800 - self.r:
+            self.vx = -k * self.vx
+            self.x -= self.r
+            num_hits += 1
+        elif self.x <= self.r:
+            self.vx = -k * self.vx
+            self.x += self.r
+            num_hits += 1
 
+        if self.y >= 600 - self.r:
+            self.vy = -k * self.vy
+            self.y -= self.r
+            num_hits += 1
+        elif self.y <= self.r:
+            self.vy = -k * self.vy
+            self.y += self.r
+            num_hits += 1
+
+    def hit_it(self, obj):
         """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
 
         Args:
@@ -70,8 +90,7 @@ class Ball:
         Returns:
             Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
         """
-        # FIXME
-        if ((self.x - obj.x)**2 + (self.y - obj.y)**2) <= obj.r:
+        if ((self.x - obj.x) ** 2 + (self.y - obj.y) ** 2) ** 0.5 <= obj.r + self.r:
             return True
         else:
             return False
@@ -97,14 +116,12 @@ class Gun:
         bullet += 1
         new_ball = Ball()
         new_ball.r += 5
-        self.f2_power = bullet
         self.angle = math.atan((event.y - new_ball.y) / (event.x - new_ball.x))
-
         new_ball.vx = self.f2_power * math.cos(self.angle)
         new_ball.vy = - self.f2_power * math.sin(self.angle)
-
         balls += [new_ball]
         self.f2_on = 0
+        self.f2_power = 10
 
     def targeting(self, event):
         """Прицеливание. Зависит от положения мыши."""
@@ -132,10 +149,8 @@ class Target:
     def __init__(self):
         self.points = 0
         self.live = 1
-    # FIXME: don't work!!! How to call this functions when object is created?
         self.id = canv.create_oval(0, 0, 0, 0)
         self.id_points = canv.create_text(30, 30, text=self.points, font='28')
-      #  self.new_target()
         """ Инициализация новой цели. """
         self.x = rnd(600, 780)
         self.y = rnd(300, 550)
@@ -153,37 +168,36 @@ class Target:
         canv.itemconfig(self.id_points, text=self.points)
 
 
-t1 = Target()
+target_1 = Target()
 screen1 = canv.create_text(400, 300, text='', font='28')
-g1 = Gun()
-#bullet = 0
-#balls = []
+gun_1 = Gun()
+bullet = 0
+balls = []
 
 
 def new_game(event=''):
-    global Gun, t1, screen1, balls, bullet
-    t1.new_target()
-    bullet = 0
-    balls = []
-    canv.bind('<Button-1>', g1.fire2_start)
-    canv.bind('<ButtonRelease-1>', g1.fire2_end)
-    canv.bind('<Motion>', g1.targeting)
+    global Gun, target_1, screen1, balls, bullet
+    target_1.new_target()
+    canv.bind('<Button-1>', gun_1.fire2_start)
+    canv.bind('<ButtonRelease-1>', gun_1.fire2_end)
+    canv.bind('<Motion>', gun_1.targeting)
 
     z = 0.03
-    t1.live = 1
-    while t1.live or balls:
+    target_1.live = 1
+    while target_1.live or balls:
         for b in balls:
             b.move()
-            if b.hit_it(t1) and t1.live:
-                t1.live = 0
-                t1.hit()
+            if b.hit_it(target_1) and target_1.live:
+                target_1.live = 0
+                target_1.hit()
                 canv.bind('<Button-1>', '')
                 canv.bind('<ButtonRelease-1>', '')
                 canv.itemconfig(screen1, text='Вы уничтожили цель за ' + str(bullet) + ' выстрелов')
+
         canv.update()
         time.sleep(0.03)
-        g1.targeting(event)
-        g1.power_up()
+        gun_1.targeting(event)
+        gun_1.power_up()
     canv.itemconfig(screen1, text='')
     canv.delete(Gun)
     root.after(750, new_game)
