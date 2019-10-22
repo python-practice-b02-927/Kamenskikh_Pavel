3995aba73434136be6705bb2e27572f63fa90c23
KPA927 <kamenskikh.pasha@mail.ru>
2019-10-22 13:42:35 +0300

    Now we can open the window and balls can move.

--- lab8/gun.py
+++ lab8/gun.py
@@ -52,15 +52,16 @@ class Ball:
         и стен по краям окна (размер окна 800х600).
         """
         # FIXME
-        self.vy += 5
+        self.vy -= 1
         self.x += self.vx
         self.y -= self.vy
-        if self.x >= 800 or self.x <= 0:
+        if self.x >= 800 - self.r or self.x <= self.r:
             self.vx = -self.vx
-        if self.y >= 600 or self.y <= 0:
+        if self.y >= 600 - self.r or self.y <= self.r:
             self.vy = -self.vy
+        self.set_coord()
 
-    def hittest(self, obj):
+    def hit_it(self, obj):
 
         """Функция проверяет сталкивалкивается ли данный обьект с целью, описываемой в обьекте obj.
 
@@ -70,6 +71,9 @@ class Ball:
             Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
         """
         # FIXME
+        if ((self.x - obj.x)**2 + (self.y - obj.y)**2) <= obj.r:
+            return True
+        else:
             return False
 
 
@@ -78,7 +82,7 @@ class Gun:
         self.f2_power = 10
         self.f2_on = 0
         self.angle = 1
-        self.id = canv.create_line(20,450,50,420,width=7) # FIXME: don't know how to set it...
+        self.id = canv.create_line(20, 450, 50, 420, width=7)
 
     def fire2_start(self, event):
         self.f2_on = 1
@@ -93,13 +97,14 @@ class Gun:
         bullet += 1
         new_ball = Ball()
         new_ball.r += 5
+        self.f2_power = bullet
         self.angle = math.atan((event.y - new_ball.y) / (event.x - new_ball.x))
+
         new_ball.vx = self.f2_power * math.cos(self.angle)
         new_ball.vy = - self.f2_power * math.sin(self.angle)
+
         balls += [new_ball]
         self.f2_on = 0
-        self.f2_power = 10
-
 
     def targeting(self, event):
         """Прицеливание. Зависит от положения мыши."""
@@ -124,21 +129,22 @@ class Gun:
 
 
 class Target:
-    self.points = 0
-    self.live = 1
+    def __init__(self):
+        self.points = 0
+        self.live = 1
     # FIXME: don't work!!! How to call this functions when object is created?
-    # self.id = canv.create_oval(0,0,0,0)
-    # self.id_points = canv.create_text(30,30,text = self.points,font = '28')
-    # self.new_target()
+        self.id = canv.create_oval(0, 0, 0, 0)
+        self.id_points = canv.create_text(30, 30, text=self.points, font='28')
+      #  self.new_target()
+        """ Инициализация новой цели. """
+        self.x = rnd(600, 780)
+        self.y = rnd(300, 550)
+        self.r = rnd(2, 50)
+        self.color = 'red'
 
     def new_target(self):
-        """ Инициализация новой цели. """
-        x = self.x = rnd(600, 780)
-        y = self.y = rnd(300, 550)
-        r = self.r = rnd(2, 50)
-        color = self.color = 'red'
-        canv.coords(self.id, x-r, y-r, x+r, y+r)
-        canv.itemconfig(self.id, fill=color)
+        canv.coords(self.id, self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
+        canv.itemconfig(self.id, fill=self.color)
 
     def hit(self, points=1):
         """Попадание шарика в цель."""
@@ -150,8 +156,8 @@ class Target:
 t1 = Target()
 screen1 = canv.create_text(400, 300, text='', font='28')
 g1 = Gun()
-bullet = 0
-balls = []
+#bullet = 0
+#balls = []
 
 
 def new_game(event=''):
@@ -168,7 +174,7 @@ def new_game(event=''):
     while t1.live or balls:
         for b in balls:
             b.move()
-            if b.hittest(t1) and t1.live:
+            if b.hit_it(t1) and t1.live:
                 t1.live = 0
                 t1.hit()
                 canv.bind('<Button-1>', '')
@@ -176,7 +182,7 @@ def new_game(event=''):
                 canv.itemconfig(screen1, text='Вы уничтожили цель за ' + str(bullet) + ' выстрелов')
         canv.update()
         time.sleep(0.03)
-        g1.targeting()
+        g1.targeting(event)
         g1.power_up()
     canv.itemconfig(screen1, text='')
     canv.delete(Gun)
